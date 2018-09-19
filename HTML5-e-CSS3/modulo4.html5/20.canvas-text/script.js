5889679c4e6f44bc8d79fb71a98f3ac58b5a4362
lucasbelizari <lucasbelizari@outlook.com>
2018-09-19 18:30:27 -0300

    Add codigos aula 20 canvas text

---
+++ HTML5-e-CSS3/modulo4.html5/20.canvas-text/script.js
@@ -0,0 +1,6 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+
+ctx.font = "30px Arial";
+ctx.fillStyle = "#FF0000";
+ctx.fillText("Oi, tudo bem? Eu sou o Lucas tenho " ,0, 400)
