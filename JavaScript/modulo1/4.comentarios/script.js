b8a1b703f3caf64202960798b155cc9d6364346a
lucasbelizari <lucasbelizari@outlook.com>
2018-10-16 19:47:14 -0300

    add codes modulo 1 javascript

---
+++ JavaScript/modulo1/4.comentarios/script.js
@@ -0,0 +1,8 @@
+//Tudo que eu digitar irá ficar cinza.
+//Exemplo: Definindo a variável do nome do usuário;
+/*Comentário
+ de
+multiplas
+ linhas*/
+//var nome = "Lucaaaaas";
+var nome = "Ciclano";
