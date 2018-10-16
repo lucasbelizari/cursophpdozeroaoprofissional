c4d699b75ac1c50086d950cdfa8b252e214272b9
lucasbelizari <lucasbelizari@outlook.com>
2018-10-16 00:14:36 -0300

    Codes aula mod 06 atividades práticas

--- HTML5-e-CSS3/modulo6.aplicacoespraticas/html_portal/assets/js/script.js
+++ HTML5-e-CSS3/modulo6.aplicacoespraticas/html_portal/assets/js/script.js
@@ -21,5 +21,16 @@ function mudarSlide(pos) {
   slideItem = pos;
   var slidewidth = document.getElementById("slideshow").offsetWidth;
   document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px"
+}
+
+function toggleMenu() {
+
+  var menu = document.getElementById("menu");
+
+  if (menu.style.display == 'none' || menu.style.display == '') {
+      menu.style.display = "block";
+  } else {
+      menu.style.display = "none";
+  }
 
 }
