7c49db54ac12f63d21a39ec36c87691aa6ca1f3b
lucasbelizari <lucasbelizari@outlook.com>
2018-10-22 15:19:44 -0300

    add codes lesson 03 eventos de teclado

--- JavaScript/modulo2.intermediario/3.eventosdeteclado/script.js
+++ JavaScript/modulo2.intermediario/3.eventosdeteclado/script.js
@@ -1,3 +1,30 @@
+/*function apertouTecla(event) {
+  console.log("Apertou alguma tecla: " +event.keyCode);
+}
 function apertouTecla(event) {
-  console.log("Apertou alguma tecla");
+  console.log("Apertou alguma tecla: " +event.keyCode);
+}
+function apertouTecla(event) {
+  console.log("Apertou alguma tecla: " +event.keyCode);
+}
+
+function apertouTecla(event) {
+  if(event.shiftKey == true)
+     console.log("Apertou alguma tecla com shift... ");
+}
+function apertouTecla(event) {
+  if(event.contrlKey == true)
+     console.log("Apertou alguma tecla com Ctrl... ");
+}
+
+function apertouTecla(event) {
+  if(event.altKey == true)
+     console.log("Apertou alguma tecla com Alt... ");
+}
+*/
+function apertouTecla(event) {
+//   console.log.log(event.)
+   if(event.shiftKey == true && event.keyCode == 69) {
+     console.log("Apertou SHIFT + E");
+   }
 }
