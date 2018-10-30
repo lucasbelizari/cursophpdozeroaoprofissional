133125bf97b48085c4dc74acdd46ea05f9d7c499
lucasbelizari <lucasbelizari@outlook.com>
2018-10-30 16:43:01 -0300

    add codes javascript

--- JavaScript/modulo3.avancado/1.temporizacao/script.js
+++ JavaScript/modulo3.avancado/1.temporizacao/script.js
@@ -1,9 +1,5 @@
 function acao() {
-     document.write("Executou...<br/>");
+  document.write("Executou...<br/>");
 }
-//setInterval(acao, 2000); 
-//a forma de parar o setInterval é colocar ele dentro de uma variável // clearInterval(timer);
-
-var timer = setInterval(acao, 2000);
-
-//setTimeout(acao,2000); // clearTimeout();
+//setInterval(acao, 250);
+setTimeout(acao, 5000);
